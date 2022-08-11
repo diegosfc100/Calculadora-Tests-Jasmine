c61b6ed577daf8c0f701a969f8a778520565e7d0
diegosfc100 <diego.sfc100@hotmail.com>
2022-08-11 12:42:45 -0300

    Cria a UI da Calculatora e captura o valor de entrada da caixa de texto

---
+++ main.js
@@ -0,0 +1,9 @@
+function calcular(event){
+    const inputValue = event.target.value;
+    const expressao = /\+|\-|\*|\//
+    const num1;
+    const num2;
+    const operador;
+}
+
+document.getElementById('inputValue').addEventListener('change', calcular);
