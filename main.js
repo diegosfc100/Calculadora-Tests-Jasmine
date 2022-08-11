e5d5f741ece686eb9aa763141f0edc7873d5d0d2
diegosfc100 <diego.sfc100@hotmail.com>
2022-08-11 12:58:51 -0300

    Extrai números e operadores da entrada da calculador

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 function calcular(event){
     const inputValue = event.target.value;
-    const expressao = /\+|\-|\*|\//
-    const num1;
-    const num2;
-    const operador;
+    const expressao = /\+|\-|\*|\//;
+
+    const nums = inputValue.split(expressao);
+    
+    const num1 = +nums[0];
+    const num2 = +nums[1];
+
+    const operacao = inputValue.match(expressao);
+    const operador = operacao[0];
+
+    const calcular = new Calcular();
+    calcular.add(num1);
+    calcular.add(num2);
+
+    console.log(calcular.total);  
+
+    //console.log(num1+num2);
 }
 
 document.getElementById('inputValue').addEventListener('change', calcular);
