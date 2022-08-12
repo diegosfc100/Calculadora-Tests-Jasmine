fde8d1d13ea056c5d73150c9b21b9db039c6ed50
diegosfc100 <diego.sfc100@hotmail.com>
2022-08-11 21:23:41 -0300

    Escreve a primeira spec do conjunto de testes para calculadora

--- calculadora.spec.js
+++ calculadora.spec.js
@@ -1,3 +1,17 @@
-descrive('calculadora.js', () => {
+describe('calculadora.js', () => {
+    it('deve adicionar um numero ao total'), () => {
 
+    }
+
+    it('deve subtrair um numero do total'), () => {
+
+    }
+
+    it('deve multiplicar um numero pelo total'), () => {
+
+    }
+
+    it('deve dividir um numero pelo total'), () => {
+
+    }
 });
